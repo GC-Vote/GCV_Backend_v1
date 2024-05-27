b54e018f6dc7d8c8d100ba4ad4bc48238085d522
passion0401lad <passion0401lad.gmail.com>
2024-05-27 22:26:50 +0900

    feat:add db migaration using dbcreate

--- src/setup/database.setup.ts
+++ src/setup/database.setup.ts
@@ -4,7 +4,8 @@ import { DBConnect, Logger } from "@/utils";
 
 const databaseSetup = async (next: () => void) => {
   try {
-    // await DBConnect.dbCreate();
+    await DBConnect.dbCreate();
+    Logger.log(MESSAGES.DATABASE.DATABASE_MIGARATION)
     await DBConnect.getConnection();
     Logger.log(MESSAGES.DATABASE.DATABASE_CONNECTION_SUCCESS);
     next();
